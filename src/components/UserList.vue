8f740e9e5033ae96dda49a2d5c4c7031e70134cc
ErlandasL <erlandaslisauskas@gmail.com>
2024-08-08 15:39:01 +0300

    Add: users list from fake api

---
+++ src/components/UserList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>User List</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">{{ user.email }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  created() {
+    this.fetchUsers();
+  },
+  methods: {
+    async fetchUsers() {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        this.users = await response.json();
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    },
+  },
+};
+</script>
